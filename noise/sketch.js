980a4ce51b2cc89be8ff661dc6a128ae8df064ed
mehmetbuber <mehmetbuber@gmail.com>
2019-02-26 17:36:28 +0300

    Save

--- noise/sketch.js
+++ noise/sketch.js
@@ -1,61 +1,115 @@
-var phase = 0;
+var heartRight = [];
+var heartLeft = [];
+var particles = [];
 
-var heart = [];
-const totalFrames = 240;
-let counter = 0;
 var heartSlider;
-let rSlider;
 
 function setup() {
-    createCanvas(1200, 800);
+    createCanvas(windowWidth, windowHeight);
     colorMode(HSB, 255);
 
-    rSlider = createSlider(0.01, 1, 1,0.01);
-    rSlider.position(20, 20);
-
+    heartSlider = createSlider(0.01, 1, 0.01, 0.01);
+    heartSlider.position(20, 20);
+    setHearth();
+    
+    particles.push(new Particle());
+    particles.push(new Particle(true));
     background(51);
 }
 
-function draw() {
-    console.log(rSlider.value());
-    background(0);
-    translate(width / 2, height / 2);
-    stroke(255);
-    noFill();
-    beginShape();
-    for (var a = 0; a < TWO_PI; a += 0.01) {
-        var xoff = cos(a + phase) + 1;
-        var yoff = sin(a + phase) + 1;
-        var r = map(noise(xoff, yoff), 0, 1, 100, 200);
-        var x = r * cos(a);
-        var y = r * sin(a);
-        vertex(x, y);
-    }
-    phase += 0.01;
-    endShape();
+function setHearth() {
+    heartRight = [];
+    heartLeft = [];
 
-    translate(-width / 2, -height / 2);
+    for (var a = PI; a > 0; a -= heartSlider.value()) {
+        var x = 16 * pow(sin(a), 3);
+        var y = -(13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+        heartRight.push(createVector(x, y));
+    }
+
+    for (var a = PI; a < TWO_PI; a += heartSlider.value()) {
+        var x = 16 * pow(sin(a), 3);
+        var y = -(13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+        heartLeft.push(createVector(x, y));
+    }
+}
+
+function draw() {
+    setHearth();
     render();
-    counter++;
 }
 
 function render() {
-    heart = [];
-    for (var i = 0; i < 1; i += rSlider.value()) {
-        var a = map(i, 0, 1, 0, TWO_PI);
-        var x = 16 * pow(sin(a), 3);
-        var y = -(13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
-        heart.push(createVector(x, y));
-    }
-
     translate(width / 2, height / 2);
-
+    background(0);
     stroke(255, 0, 200);
     strokeWeight(4);
     noFill();
-    beginShape();
-    for (let v of heart) {
-        vertex(10 * v.x, 10 * v.y);
+
+    //beginShape();
+    //for (let v of heartRight) {
+    //    vertex(10 * v.x, 10 * v.y);
+    //}
+    //endShape();
+
+    //beginShape();
+    //for (let v of heartLeft) {
+    //    vertex(10 * v.x, 10 * v.y);
+    //}
+    //endShape();
+
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].draw();
+        particles[i].move();
     }
-    endShape();
+}
+
+function Particle(right) {
+    this.right = right;
+    this.index = 0;
+    this.r = 2;
+    this.speed = 1;
+    this.red = 255;
+    this.green = 255;
+    this.blue = 255;
+    this.alpha = 100;
+    this.path = [];
+    this.totalLength = 100;
+
+    this.draw = function () {
+        if (this.right) {
+            var next = {
+                x: heartRight[this.index].x * 10,
+                y: heartRight[this.index].y * 10
+            };
+            this.path.push(next);
+        } else {
+            var next = {
+                x: heartLeft[this.index].x * 10,
+                y: heartLeft[this.index].y * 10
+            };
+            this.path.push(next);
+        }
+        strokeWeight(this.r );
+
+        this.r += 0.01;
+        beginShape();
+        for (var i = 0; i < this.path.length; i++) {
+            var ratio = i / this.totalLength;
+            stroke(this.red, this.green, this.blue, this.alpha * ratio);
+            vertex(this.path[i].x, this.path[i].y);
+        }
+        endShape();
+
+        if (this.path.length > this.totalLength)
+            this.path.shift();
+    };
+
+    this.move = function() {
+        this.index++;
+        if (this.index > heartRight.length - 1) {
+            this.index = 0;
+            this.path = [];
+        }
+    };
 }
